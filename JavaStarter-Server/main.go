f8e36b94c6822f758657ad9b61ea5e66d6c713cf
Zerounary <850657663@qq.com>
2021-01-14 18:38:41 +0800

    添加示例程序

--- JavaStarter-Server/main.go
+++ JavaStarter-Server/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/panjf2000/gnet"
 )
 
+type echoServer struct {
+	*gnet.EventServer
+}
+
+func (es *echoServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.Action) {
+	out = frame
+	return
+}
+
 func main() {
-	fmt.Println("This is JavaStarter-Server")
+	echo := new(echoServer)
+	log.Fatal(gnet.Serve(echo, "tcp://:9000", gnet.WithMulticore(true)))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/Zerounary/JavaStarter
 
 go 1.15
+
+require (
+	github.com/panjf2000/gnet v1.3.2
+	github.com/smallnest/goframe v1.0.0
+	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
+)
